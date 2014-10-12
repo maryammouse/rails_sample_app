74e28927e8e9a5b42e53cbed8d5cffa454cbf778
Maryam Syed <maryamsyed2096@gmail.com>
2014-10-11 17:15:06 -0700

    Add signup functionality

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,30 @@ describe "User pages" do
         it { should have_title(full_title('Sign up')) }
     end
 
+    describe "signup" do
+      before { visit signup_path } 
+        let(:submit) { "Create my account" }
+        
+        describe "signup data is invalid" do
+          it "should not create a user" do 
+            expect { click_button submit }.not_to change(User, :count)
+          end 
+        end
+
+        describe "signup data is valid" do
+          before do
+            fill_in "Name", with: "Example User"
+            fill_in "Email", with: "user@example.com"
+            fill_in "Password", with: "foobar"
+            fill_in "Confirmation", with: "foobar"
+          end
+
+          it "should create a user" do
+            expect { click_button submit }.to change(User, :count).by(1)
+          end
+        end
+    end
+
     describe "profile page" do
       let(:user) { FactoryGirl.create(:user) }
       before { visit user_path(user) }
@@ -18,4 +42,5 @@ describe "User pages" do
       it { should have_content(user.name) }
       it { should have_title(user.name) }
     end 
+
 end
